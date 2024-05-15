c1074e781cc5e2eda0c5f7ba3fcbcc73c6953fba
Ferhat <parhat002@gmail.com>
2024-05-15 08:08:37 +0300

    data-ModelMapper

--- src/main/java/com/cydeo/mapper/UserMapper.java
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -17,7 +17,7 @@ public class UserMapper {
         return modelMapper.map(dto, User.class);
     }
 
-    public UserDTO convertToEntity(User entity){
+    public UserDTO convertToDto(User entity){
         return modelMapper.map(entity, UserDTO.class);
     }
 }
