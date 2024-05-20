841dbaaafefc15742cb259f8f4bd3af26672b741
Ferhat <parhat002@gmail.com>
2024-05-20 23:37:42 +0300

    Task

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,12 +1,17 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -21,26 +26,40 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+        List<Task> tasks=taskRepository.findAll();
+        return tasks.stream().map(taskMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public void save(TaskDTO dto) {
-
+        dto.setTaskStatus(Status.OPEN);
+        dto.setAssignedDate(LocalDate.now());
+        taskRepository.save(taskMapper.convertToEntity(dto));
     }
 
     @Override
     public void update(TaskDTO dto) {
-
+        Optional<Task> task = taskRepository.findById(dto.getId());
+        Task convertedTask = taskMapper.convertToEntity(dto);
+        if (task.isPresent()){
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            taskRepository.save(convertedTask);
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()){
+            task.get().setIsDeleted(true);
+            taskRepository.save(task.get());
+        }
     }
 
     @Override
     public TaskDTO findById(Long id) {
-        return null;
+        Optional<Task> task = taskRepository.findById(id);
+        return task.map(taskMapper::convertToDto).orElse(null);
     }
 }
