2f5c641a72a8a75134963acb839114170d7f8658
Ferhat <parhat002@gmail.com>
2024-06-03 20:58:01 +0300

    Spring_Security

---
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//        return new
+//    }
+}
