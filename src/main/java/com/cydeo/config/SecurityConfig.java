312189c33e067ee01308a88b3074b05d2abf60ca
Ferhat <parhat002@gmail.com>
2024-06-04 20:13:49 +0300

    SecurityFilterChain

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,4 +29,33 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(userList);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/**").hasRole("ADMIN")
+                        .requestMatchers("/project/**").hasRole("MANAGER")
+                        .requestMatchers("/task/employee/**").hasRole("EMPLOYEE")
+                        .requestMatchers("/task/**").hasRole("MANAGER")
+                        .requestMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/fragments/**",
+                                "/assets/**",
+                                "/images/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin( form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/welcome")
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                ).build();
+
+    }
+
 }
